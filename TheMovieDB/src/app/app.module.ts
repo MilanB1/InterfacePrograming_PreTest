297278f246ae0f42a4c651c4352e43a49838e5e9
Milan Bacovic <milanbacovic@Milans-MacBook-Pro.local>
2019-09-10 12:42:45 +1000

    nista

--- TheMovieDB/src/app/app.module.ts
+++ TheMovieDB/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { MovieInfoComponentComponent } from './movie-info-component/movie-info-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    FooterBarComponent
+    FooterBarComponent,
+    MovieInfoComponentComponent
   ],
   imports: [
     BrowserModule
